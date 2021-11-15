6802de5ab0437150ed685980c5356b3061c6ae4a
Jane <janechodance@gmail.com>
2021-11-15 12:50:37 -0500

    commit tree 2eb250cc0a250b0d13169542d9b5d64c02e2f5dd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,27 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+  post '/baked_goods' do
+    new_baked_goods = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    new_baked_goods.to_json
+  end
+  patch '/bakeries/:id' do
+    update_bakery = Bakery.find(params[:id])
+    update_bakery.update(
+      name: params[:name]
+    )
+    update_bakery.to_json
+  end
+  delete '/baked_goods/:id' do
+    baked_good_delete = BakedGood.find(params[:id])
+    # delete the review
+    baked_good_delete.destroy
+    # send a response with the deleted review as JSON
+    baked_good_delete.to_json
+  end
 
 end
